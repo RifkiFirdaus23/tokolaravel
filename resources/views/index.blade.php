04b305670bc7db9da47ea34909025f5960fdacc8
RifkiFirdaus23 <rifkifirdaus0002gmail.com>
2019-05-12 17:43:18 +0700

    asfasfaf

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -102,8 +102,6 @@
 
 <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="{{asset('js/jquery-3.4.1.js')}}"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="{{asset('js/Bootstrap.js')}}"></script>
   </body>
 </html>
